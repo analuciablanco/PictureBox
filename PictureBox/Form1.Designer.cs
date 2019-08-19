f734cbfab1ffd3a278aa93f1320ea9bc2753ae02
Ana Lucia Blanco <analuciablanco@outlook.com>
2019-08-19 11:46:44 -0700

    Se añadió botón para escala de grises.

--- PictureBox/Form1.Designer.cs
+++ PictureBox/Form1.Designer.cs
@@ -31,12 +31,15 @@
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(21, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 63);
             this.button1.TabIndex = 0;
@@ -46,22 +49,43 @@
             // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(12, 81);
+            this.imageBox1.Location = new System.Drawing.Point(21, 81);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(776, 357);
+            this.imageBox1.Size = new System.Drawing.Size(371, 357);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(408, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 63);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Escala grises";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(408, 81);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(371, 357);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.imageBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +94,8 @@
 
         private System.Windows.Forms.Button button1;
         private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button2;
+        private Emgu.CV.UI.ImageBox imageBox2;
     }
 }
 
--- PictureBox/Form1.cs
+++ PictureBox/Form1.cs
@@ -16,7 +16,8 @@ namespace PictureBox
 {
     public partial class Form1 : Form
     {
-        Image<Bgr, byte> _InputColor;
+        Image<Bgr, byte> InputColor;
+        Image<Gray, byte> InputGray;
 
         public Form1()
         {
@@ -26,15 +27,28 @@ namespace PictureBox
         private void Button1_Click(object sender, EventArgs e)
         {
             string rutaImagen = "E:\\tomlinson.jpg";
-           _InputColor = new Image<Bgr, byte>(rutaImagen);
+           InputColor = new Image<Bgr, byte>(rutaImagen);
 
-            if (_InputColor == null)
+            if (InputColor == null)
             {
                 MessageBox.Show("No cargó la imagen correctamente.");
                 return;
             }
 
-            imageBox1.Image = _InputColor;
+            imageBox1.Image = InputColor;
+
+            // Desactiva la existencia de TODAS las opciones cuando le damos clic derecho,
+            // Pero con esta línea podemos definit qué opciones poner o quitar todas.
+            imageBox1.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.PanAndZoom;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            // Convertir a grises.
+            InputGray = InputColor.Convert<Gray, byte>();
+
+            imageBox2.Image = InputGray;
+            imageBox1.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.PanAndZoom;
         }
     }
 }
